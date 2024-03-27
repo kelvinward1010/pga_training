cfb1605d4c85e07e89d2b4ce8552d92e810d35e2
Kelvin Ward <kelvinward1010@gmail.com>
2024-03-27 16:22:24 +0700

    update more

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    // proxy: {
+    //   // Cấu hình proxy cho các yêu cầu bắt đầu với '/api'
+    //   '/api': {
+    //     target: 'http://api.training.div3.pgtest.co', // Địa chỉ của backend server
+    //     changeOrigin: true,
+    //     secure: false, // Đặt là false nếu backend server không có SSL
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
   },
 })
